9aa9e41ace0aa8d5ec2d72cb33ca340cdd5bc236
unknown <edoyangz@gmail.com>
2024-10-10 14:53:43 +1100

    update

--- stocker-dash/src/App.jsx
+++ stocker-dash/src/App.jsx
@@ -1,63 +1,19 @@
 import { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import AddProduct from "./pages/AddProduct/AddProduct";
+import Test from "./pages/Test";
 
 function App() {
-  const [formData, setFormData] = useState({
-    productName: "",
-    productBarcode: "",
-    productImage: "",
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Make the POST request with Axios
-    axios
-      .post("http://localhost:5000/api/products", formData)
-      .then((response) => {
-        console.log("Product submitted successfully:", response.data);
-        alert("Product submitted successfully!");
-      })
-      .catch((error) => {
-        console.error("There was an error submitting the product:", error);
-      });
-  };
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="productName"
-          placeholder="Product Name"
-          value={formData.productName}
-          onChange={handleInputChange}
-        />
-        <input
-          type="number"
-          name="productBarcode"
-          placeholder="Product Barcode"
-          value={formData.productBarcode}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="productImage"
-          placeholder="Product Image"
-          value={formData.productImage}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/add-product" Component={AddProduct} />
+        <Route path="/test" Component={Test} />
+      </Routes>
     </>
   );
 }
---
+++ stocker-dash/src/pages/Test.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import axios from "axios";
+
+function Test() {
+  const [barcode, setBarcode] = useState("");
+  const [isScannerOpen, setIsScannerOpen] = useState(false);
+
+  const [formData, setFormData] = useState({
+    productName: "",
+    productBarcode: "",
+    productImage: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const finalData = {
+      ...formData,
+      productBarcode: barcode,
+    };
+
+    axios
+      .post("http://localhost:5000/api/products", finalData)
+      .then((response) => {
+        console.log("Product submitted successfully:", response.data);
+        alert("Product submitted successfully!");
+      })
+      .catch((error) => {
+        console.error("There was an error submitting the product:", error);
+      });
+  };
+
+  const handleScan = (err, result) => {
+    if (result) {
+      setBarcode(result.text);
+      setIsScannerOpen(false);
+    }
+  };
+
+  const handleBarcodeClick = () => {
+    const confirmUseCamera = window.confirm(
+      "Do you want to use the camera for scanning?"
+    );
+    if (confirmUseCamera) {
+      setIsScannerOpen(true);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="input">
+          <label htmlFor="productName">Product Name</label>
+          <input
+            type="text"
+            name="productName"
+            placeholder="Product Name"
+            value={formData.productName}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="input">
+          <label htmlFor="productBarcode">Product Barcode</label>
+          <input
+            type="text"
+            name="productBarcode"
+            id="productBarcode"
+            placeholder="Product Barcode"
+            value={barcode}
+            onChange={(e) => setBarcode(e.target.value)}
+            onClick={() => handleBarcodeClick()}
+          />
+        </div>
+
+        <div className="input">
+          <label htmlFor="productImage">Product Image</label>
+          <input
+            type="text"
+            name="productImage"
+            placeholder="Product Image"
+            value={formData.productImage}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      {isScannerOpen && (
+        <BarcodeScannerComponent
+          width={500}
+          height={500}
+          onUpdate={handleScan}
+        />
+      )}
+    </>
+  );
+}
+
+export default Test;
